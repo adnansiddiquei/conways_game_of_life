afa1635333bef76c77a4d4ca3e7f53ace151379b
adnansiddiquei <adnanshirik@gmail.com>
2024-03-11 16:07:39 +0000

    Created 2 functions `parse_arg` and `parse_arg_int` which can be used to parse options given into the command line.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,93 @@
+#include <mpi.h>
+
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <utility>
 
 #include "array2d.h"
 
+/**
+ * Parses the command line arguments passed into this script, and returns the value (as
+ * a std::string) of the requested option.
+ *
+ * An empty string "" is returned if the option is not required and was not provided.
+ *
+ * @param argc The count of arguments passed into the script.
+ * @param argv The values passed into the script.
+ * @param option The option to retrieve the value for.
+ * @param required Whether the specified option is required. If it is required, the
+ * program will exit with an appropriate error message.
+ *
+ * @return The value of the requested option.
+ */
+std::string parse_arg(int &argc, char *argv[], std::string option, bool required) {
+    for (int i = 0; i < argc; i++) {
+        std::string current_option = argv[i];
+
+        if (current_option == option) {
+            if (i + 1 < argc) {
+                return argv[i + 1];
+            } else {
+                std::cerr << option << " option requires one argument." << std::endl;
+                std::exit(1);
+            }
+        }
+    }
+
+    // if code reaches here then we are looking for an argument that was not passed into
+    // the terminal
+    if (required) {
+        // exit with an error if the option is required
+        std::string errorMessage = option + " option must be provided.";
+        std::cerr << errorMessage << std::endl;
+        std::exit(1);
+    } else {
+        return "";
+    }
+}
+
+/**
+ * Parses the command line arguments passed into this script, and returns the value (as
+ * an int) of the requested option.
+ *
+ * `0` is returned if the option is not required and was not provided.
+ *
+ * @param argc The count of arguments passed into the script.
+ * @param argv The values passed into the script.
+ * @param option The option to retrieve the value for.
+ * @param required Whether the specified option is required. If it is required, the
+ * program will exit with an appropriate error message.
+ *
+ * @return The value (as an int) of the requested option.
+ *
+ */
+int parse_arg_int(int &argc, char *argv[], std::string option, bool required) {
+    std::string arg = parse_arg(argc, argv, option, required);
+
+    // If the option is not required and has not been provided, return a 0.
+    if (!required & arg == "") {
+        return 0;
+    }
+
+    // try a type conversion to int, but raise the  appropriate error if this fails
+    try {
+        return std::stoi(arg);
+    } catch (const std::invalid_argument &e) {
+        std::cerr << "Invalid argument: " << option << " argument must an integer."
+                  << std::endl;
+        std::exit(1);
+    } catch (const std::out_of_range &e) {
+        // Handle case where the string represents a number outside the range of int
+        std::cerr << "Out of range: " << option << " argument is too large."
+                  << std::endl;
+        std::exit(1);
+    }
+}
+
 int main(int argc, char *argv[]) {
-    array2d::Array2D<int> arr(10, 10);
-    array2d::Array2D<int> arr_transposed(10, 10);
-    arr(0, 1) = 511;
+    int grid_size = parse_arg_int(argc, argv, "--grid-size", true);
+    int random_seed = parse_arg_int(argc, argv, "--random-seed", false);
 
-    std::cout << arr(0, 1) << std::endl;
 
-    array2d::transpose(arr_transposed, arr);
-
-    std::cout << arr_transposed(1, 0) << std::endl;
-
-    return 0;
 }
