835673b42e3d834bdf40f82714adb6a77617af62
adnansiddiquei <adnanshirik@gmail.com>
2024-03-11 01:46:51 +0000

    Completed implementation of Array2D and added a transpose function.
    - Transpose function writes contiguously.
    - Set up relevant CMAKE files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#include "array2d.h"
+#include <iostream>
+
 
 int main(int argc, char *argv[]) {
+    array2d::Array2D<int> arr(10, 10);
+    array2d::Array2D<int> arr_transposed(10, 10);
+    arr(0, 1) = 511;
+
+    std::cout << arr(0, 1) << std::endl;
+
+    array2d::transpose(arr_transposed, arr);
+
+    std::cout << arr_transposed(1, 0) << std::endl;
+
     return 0;
 }
--- src/utils/include/array2d.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Adnan Siddiquei on 10/03/2024.
-//
-
-#ifndef AS3438_ARRAY2D_H
-#define AS3438_ARRAY2D_H
-
-#endif //AS3438_ARRAY2D_H
