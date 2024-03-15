e75b9ad743c12b40d1686f0c0c3fb96670123445
adnansiddiquei <adnanshirik@gmail.com>
2024-03-15 19:53:01 +0000

    Added tests for array2d::Array2DWithHalo.

--- tests/test_array2d.cpp
+++ tests/test_array2d.cpp
@@ -8,14 +8,50 @@
 #include "../src/array2d/include/array2d.h"
 
 /**
- * Tests basic array operations such as creating, editing and using class methods.
+ * Test array2d::Array2D
  */
-TEST(array2d, array_operations) {
-    array2d::Array2D<int> arr(10, 10);
-    arr(7, 2) = 43;
+TEST(array2D, Array2D) {
+    int n_rows = 10, n_cols = 12, intial_value = 22;
 
-    EXPECT_EQ(arr.get_cols(), 10);
-    EXPECT_EQ(arr.get_rows(), 10);
-    EXPECT_EQ(arr.get_size(), 100);
+    // Basic operations
+    array2d::Array2D<int> arr(n_rows, n_cols, intial_value);
+
+    EXPECT_EQ(arr.get_rows(), n_rows);
+    EXPECT_EQ(arr.get_cols(), n_cols);
+    EXPECT_EQ(arr.get_size(), n_rows * n_cols);
+
+    // Make sure the initial value works
+    for (int i = 0; i < n_rows; i++) {
+        for (int j = 0; j < n_cols; j++) {
+            EXPECT_EQ(arr(i, j), intial_value);
+        }
+    }
+
+    arr(7, 2) = 43;
     EXPECT_EQ(arr(7, 2), 43);
+}
+
+/**
+ * Test array2d::Array2DWithHalo
+ */
+TEST(array2D, Array2DWithHalo) {
+    int n_rows = 8, n_cols = 16;
+
+    // Basic operations
+    array2d::Array2DWithHalo<int> arr(n_rows, n_cols);
+
+    EXPECT_EQ(arr.get_rows(), n_rows);
+    EXPECT_EQ(arr.get_cols(), n_cols);
+    EXPECT_EQ(arr.get_size(), n_rows * n_cols);
+
+    arr(7, 2) = 22;
+    EXPECT_EQ(arr(7, 2), 22);
+
+    // Make sure there is a halo
+    for (int i = -1; i < n_rows + 1; i++) {
+        for (int j = -1; j < n_cols + 1; j++) {
+            arr(i, j) = 67;
+            EXPECT_EQ(arr(i, j), 67);
+        }
+    }
 }
